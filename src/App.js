0aea920d9d3d053ea4ad91c9d4b2410fcd95df2d
Gustavo Baccan Gomes <gustavo.gomes@butantan.gov.br>
2019-05-21 16:05:57 -0300

    Aula Caminhos relativos Completa

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react'
 
-import Routes from './routes'
+import TodoItem from './components/TodoList/TodoItem'
 
-const App = () => <Routes />
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+
+            </div>
+        )
+    }
+}
 
 export default App
